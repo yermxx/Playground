3a61ab458e5688f479084372051aefb4494ad1a2
yermxx <66yrlee99@gmail.com>
2024-11-23 23:43:03 +0900

    feat: Update form validator rules and error messages

--- vanilla-js/01-form-validator/script.js
+++ vanilla-js/01-form-validator/script.js
@@ -4,32 +4,52 @@ const email = document.getElementById('email');
 const password = document.getElementById('password');
 const confirmPw = document.getElementById('confirm-pw');
 
-function showError(input, message) {
+const showError = (input, message) => {
   const formControl = input.parentElement;
   formControl.className = 'form-control error';
   const small = formControl.querySelector('small');
   small.innerText = message;
-}
+};
 
-function showSuccess(input) {
+const showSuccess = (input) => {
   const formControl = input.parentElement;
   formControl.className = 'form-control success';
-}
+};
 
-function isVaildEmail(email) {
+const checkEmail = (input) => {
   const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-  return emailPattern.test(String(email).toLowerCase());
-}
+  if (emailPattern.test(String(input.value).toLowerCase())) showSuccess(input);
+  else showError(input, 'Email is not valid');
+};
+
+const checkRequired = (inputArr) => {
+  inputArr.forEach(function (input) {
+    if (!input.value.trim()) showError(input, `${getFieldName(input)} is required`);
+    else showSuccess(input);
+  });
+};
+
+const checkLength = (input, min, max) => {
+  if (input.value.length < min) showError(input, `${getFieldName(input)} must be at least ${min} characters`);
+  else if (input.value.length > max) showError(input, `${getFieldName(input)} must be less than ${max} characters`);
+  else showSuccess(input);
+};
+
+const checkPasswordMatch = (input1, input2) => {
+  if (input1.value !== input2.value) showError(input2, 'Passwords do not match');
+};
+
+const getFieldName = (input) => {
+  if (input.id === 'confirm-pw') return 'Confirm password';
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+};
 
 form.addEventListener('submit', function (e) {
   e.preventDefault();
 
-  !username.value ? showError(username, 'Username is required') : showSuccess(username);
-  !email.value
-    ? showError(email, 'Email is required')
-    : !isVaildEmail(email.value)
-      ? showError(email, 'Email is not vaild')
-      : showSuccess(email);
-  !password.value ? showError(password, 'Password is required') : showSuccess(password);
-  !confirmPw.value ? showError(confirmPw, 'Confirm password is required') : showSuccess(confirmPw);
+  checkRequired([username, email, password, confirmPw]);
+  checkLength(username, 3, 15);
+  checkLength(password, 6, 25);
+  checkEmail(email);
+  checkPasswordMatch(password, confirmPw);
 });
